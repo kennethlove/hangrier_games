534436c422a8f19debf8997c7368241842d4cd5a
Kenneth Love <kennethlove@gmail.com>
2025-01-21 21:51:05 -0800

    small tweak

--- web/src/cache.rs
+++ web/src/cache.rs
@@ -13,6 +13,7 @@ pub(crate) enum QueryError {
     GameNotFound(String),
     NoGames,
     Unknown,
+    BadJson,
 }
 
 #[derive(PartialEq, Debug)]
--- web/src/components/games_list.rs
+++ web/src/components/games_list.rs
@@ -1,17 +1,22 @@
 use crate::cache::{QueryError, QueryKey, QueryValue};
 use crate::components::{CreateGameButton, CreateGameForm, DeleteGameModal, GameDelete};
+use crate::routes::Routes;
 use dioxus::prelude::*;
 use dioxus_query::prelude::{use_get_query, use_query_client, QueryResult};
 use game::games::Game;
-use crate::routes::Routes;
 
 async fn fetch_games(keys: Vec<QueryKey>) -> QueryResult<QueryValue, QueryError> {
     if let Some(QueryKey::AllGames) = keys.first() {
-        let response = reqwest::get("http://127.0.0.1:3000/api/games")
-            .await.unwrap()
-            .json::<Vec<Game>>()
-            .await.unwrap();
-        QueryResult::Ok(QueryValue::Games(response))
+        match reqwest::get("http://127.0.0.1:3000/api/games").await {
+            Ok(request) => {
+                if let Ok(response) = request.json::<Vec<Game>>().await {
+                    QueryResult::Ok(QueryValue::Games(response))
+                } else {
+                    QueryResult::Err(QueryError::BadJson)
+                }
+            },
+            _ => QueryResult::Err(QueryError::NoGames)
+        }
     } else {
         QueryResult::Err(QueryError::Unknown)
     }

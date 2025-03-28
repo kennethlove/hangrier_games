8f031483601c93f32e634a9ddcfbeb81b9fd98e6
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2025-03-28 12:56:28 -0700

    Basic accordion for game day details

--- web/src/components/game_detail.rs
+++ web/src/components/game_detail.rs
@@ -1,16 +1,18 @@
+use crate::API_HOST;
 use crate::cache::{MutationError, MutationValue, QueryError, QueryKey, QueryValue};
+use crate::components::full_game_log::FullGameLog;
 use crate::components::game_areas::GameAreaList;
 use crate::components::game_edit::GameEdit;
 use crate::components::game_tributes::GameTributes;
-use crate::API_HOST;
 use dioxus::prelude::*;
-use dioxus_query::prelude::{use_get_query, use_mutation, use_query_client, MutationResult, QueryResult};
+use dioxus_query::prelude::{
+    MutationResult, QueryResult, use_get_query, use_mutation, use_query_client,
+};
 use game::games::Game;
 use game::games::GameStatus;
 use game::tributes::Tribute;
 use reqwest::StatusCode;
 use std::ops::Deref;
-use crate::components::full_game_log::FullGameLog;
 
 async fn fetch_game(keys: Vec<QueryKey>) -> QueryResult<QueryValue, QueryError> {
     if let Some(QueryKey::Game(identifier)) = keys.first() {
@@ -36,23 +38,17 @@ async fn next_step(identifier: String) -> MutationResult<MutationValue, Mutation
 
     let response = client
         .put(url)
-        .send().await.expect("Failed to advance game");
+        .send()
+        .await
+        .expect("Failed to advance game");
 
     dioxus_logger::tracing::info!("{:?}", &response);
 
     match response.status() {
-        StatusCode::NO_CONTENT => {
-            MutationResult::Ok(MutationValue::GameFinished(identifier))
-        }
-        StatusCode::CREATED => {
-            MutationResult::Ok(MutationValue::GameStarted(identifier))
-        }
-        StatusCode::OK => {
-            MutationResult::Ok(MutationValue::GameAdvanced(identifier))
-        }
-        _ => {
-            MutationResult::Err(MutationError::UnableToAdvanceGame)
-        }
+        StatusCode::NO_CONTENT => MutationResult::Ok(MutationValue::GameFinished(identifier)),
+        StatusCode::CREATED => MutationResult::Ok(MutationValue::GameStarted(identifier)),
+        StatusCode::OK => MutationResult::Ok(MutationValue::GameAdvanced(identifier)),
+        _ => MutationResult::Err(MutationError::UnableToAdvanceGame),
     }
 }
 
@@ -60,7 +56,7 @@ async fn next_step(identifier: String) -> MutationResult<MutationValue, Mutation
 fn GameStatusState() -> Element {
     let game_signal: Signal<Option<Game>> = use_context();
     let game = game_signal.read();
-    
+
     if let Some(game) = game.clone() {
         let game_next_step: String;
 
@@ -88,13 +84,19 @@ fn GameStatusState() -> Element {
         let game_name = game.name.clone();
         let game_day = game.day.unwrap_or(0);
         let game_finished = game.status == GameStatus::Finished;
-        let tribute_count = game.clone()
-            .tributes.into_iter()
+        let tribute_count = game
+            .clone()
+            .tributes
+            .into_iter()
             .filter(|t| t.is_alive())
             .collect::<Vec<Tribute>>()
             .len();
         let winner_name = {
-            if game.winner().is_some() { game.winner().unwrap().name } else { String::new() }
+            if game.winner().is_some() {
+                game.winner().unwrap().name
+            } else {
+                String::new()
+            }
         };
         let g = game.clone();
 
@@ -108,21 +110,19 @@ fn GameStatusState() -> Element {
                 mutate.manual_mutate(game_id.clone()).await;
 
                 match mutate.result().deref() {
-                    MutationResult::Ok(mutation_result) => {
-                        match mutation_result {
-                            MutationValue::GameAdvanced(game_identifier) => {
-                                client.invalidate_queries(&[QueryKey::Game(game_identifier.into())]);
-                            }
-                            MutationValue::GameFinished(_) => {
-                                game.end();
-                            }
-                            MutationValue::GameStarted(game_identifier) => {
-                                game.start();
-                                client.invalidate_queries(&[QueryKey::Game(game_identifier.into())]);
-                            }
-                            _ => {}
+                    MutationResult::Ok(mutation_result) => match mutation_result {
+                        MutationValue::GameAdvanced(game_identifier) => {
+                            client.invalidate_queries(&[QueryKey::Game(game_identifier.into())]);
                         }
-                    }
+                        MutationValue::GameFinished(_) => {
+                            game.end();
+                        }
+                        MutationValue::GameStarted(game_identifier) => {
+                            game.start();
+                            client.invalidate_queries(&[QueryKey::Game(game_identifier.into())]);
+                        }
+                        _ => {}
+                    },
                     MutationResult::Err(MutationError::UnableToAdvanceGame) => {
                         dioxus_logger::tracing::error!("Failed to advance game");
                     }
@@ -133,22 +133,36 @@ fn GameStatusState() -> Element {
 
         rsx! {
             div {
-                class: "flex flex-col gap-2 border p-2",
+                class: "flex flex-col gap-2 mt-4",
                 div {
-                    class: "flex flex-row gap-4 place-content-between",
+                    class: "flex flex-row flex-wrap gap-2 place-content-between",
                     h2 {
-                        class: "text-xl",
+                        class: "text-2xl cinzel-font theme1:text-amber-300",
                         "{game_name}"
+                        span {
+                            class: "pl-2",
+                            GameEdit {
+                                identifier: g.identifier,
+                                name: g.name,
+                                icon_class: "fill-amber-600 size-4"
+                            }
+                        }
                     }
                     div {
-                        class: "flex flex-row gap-2",
-                        GameEdit {
-                            identifier: g.identifier,
-                            name: g.name,
-                            icon_class: "fill-red-600"
-                        }
+                        class: "flex flex-row gap-2 place-content-center sm:place-content-end flex-grow",
                         button {
-                            class: "button border px-2 py-1",
+                            class: r#"
+                            py-1
+                            px-2
+                            border
+                            whitespace-nowrap
+                            cursor-pointer
+                            bg-radial
+                            theme1:from-amber-300
+                            theme1:to-red-600
+                            theme1:border-red-600
+                            theme1:text-red-900
+                            "#,
                             onclick: next_step_handler,
                             disabled: game_finished,
                             "{game_next_step}"
@@ -157,28 +171,34 @@ fn GameStatusState() -> Element {
                 }
 
                 if !winner_name.is_empty() {
-                    h1 { "Winner: {winner_name}!"}
+                    h1 {
+                        class: "text-xl",
+                        "Winner: {winner_name}!"
+                    }
                 }
 
                 div {
                     class: "flex flex-row place-content-between",
                     p {
+                        class: "theme1:text-stone-200",
                         span {
-                            class: "block text-sm",
+                            class: "block text-sm theme1:text-stone-800 font-semibold",
                             "status"
                         }
                         "{game_status}"
                     }
                     p {
+                        class: "theme1:text-stone-200",
                         span {
-                            class: "block text-sm",
+                            class: "block text-sm theme1:text-stone-800 font-semibold",
                             "day"
                         }
                         "{game_day}"
                     }
                     p {
+                        class: "theme1:text-stone-200",
                         span {
-                            class: "block text-sm",
+                            class: "block text-sm theme1:text-stone-800 font-semibold",
                             "tributes alive"
                         }
                         "{tribute_count}"
@@ -200,7 +220,6 @@ pub fn GamePage(identifier: String) -> Element {
         }
         GameDetailPage { identifier }
     }
-
 }
 
 #[component]
@@ -234,39 +253,132 @@ pub fn GameDetailPage(identifier: String) -> Element {
 pub fn GameDetails(game: Game) -> Element {
     rsx! {
         div {
-            div {
-                class: "grid grid-cols-2 gap-8 lg:grid-cols-3 2xl:grid-cols-5",
-
-                div {
-                    class: "flex flex-col gap-4 2xl:col-span-2",
-                    div {
-                        h3 {
-                            class: "text-xl mb-2",
-                            "Areas",
-                            i {
-                                class: "ra ra-aura"
+            class: r#"
+            grid
+            grid-cols-none
+            sm:grid-cols-2
+            gap-8
+            lg:grid-cols-3
+            2xl:grid-cols-5
+            "#,
+            details {
+                class: r#"
+                px-2
+                group
+                transition
+                duration-500
+                theme1:bg-red-900
+                theme1:to-red-200
+                theme1:from-red-900
+                "#,
+                summary {
+                    class: r#"
+                    p-4
+                    flex
+                    items-center
+                    justify-between
+                    cursor-pointer
+                    "#,
+                    h3 {
+                        class: "block text-xl mb-2 transition theme1:group-open:text-amber-600",
+                        "Areas",
+                    }
+                    span {
+                        class: "sm:invisible transition group-open:rotate-180",
+                        svg {
+                            class: "h-5 w-5 fill-none stroke-current theme1:group-open:stroke-amber-600",
+                            view_box: "0 0 24 24",
+                            path {
+                                stroke_linecap: "round",
+                                stroke_linejoin: "round",
+                                stroke_width: "2",
+                                d: "M19 9l-7 7-7-7"
                             }
                         }
-                        GameAreaList { }
-                    }
-
-                    div {
-                        h3 {
-                            class: "text-xl mb-2",
-                            "Day log"
-                        }
-                        FullGameLog { day: game.day.unwrap_or_default() }
                     }
                 }
-                div {
-                    class: "lg:grid-cols-subgrid lg:col-span-2 2xl:col-span-3 lg:grid-cols-2 2xl:grid-cols-3",
+                GameAreaList { }
+            }
+
+            details {
+                class: r#"
+                px-2
+                group
+                transition
+                duration-500
+                theme1:bg-red-900
+                theme1:to-red-200
+                theme1:from-red-900
+                "#,
+                summary {
+                    class: r#"
+                    p-4
+                    flex
+                    items-center
+                    justify-between
+                    cursor-pointer
+                    "#,
                     h3 {
-                        class: "text-xl mb-2",
+                        class: "block text-xl mb-2 transition theme1:group-open:text-amber-600",
                         "Tributes"
                     }
-
-                    GameTributes { }
+                    span {
+                        class: "sm:invisible transition group-open:rotate-180",
+                        svg {
+                            class: "h-5 w-5 fill-none stroke-current theme1:group-open:stroke-amber-600",
+                            view_box: "0 0 24 24",
+                            path {
+                                stroke_linecap: "round",
+                                stroke_linejoin: "round",
+                                stroke_width: "2",
+                                d: "M19 9l-7 7-7-7"
+                            }
+                        }
+                    }
                 }
+
+                GameTributes { }
+            }
+
+            details {
+                class: r#"
+                px-2
+                group
+                transition
+                duration-500
+                theme1:bg-red-900
+                theme1:to-red-200
+                theme1:from-red-900
+                "#,
+                open: true,
+                summary {
+                    class: r#"
+                    p-4
+                    flex
+                    items-center
+                    justify-between
+                    cursor-pointer
+                    "#,
+                    h3 {
+                        class: "block text-xl mb-2 transition theme1:group-open:text-amber-600",
+                        class: "text-xl mb-2",
+                        "Day log"
+                    }
+                    span {
+                        class: "sm:invisible transition group-open:rotate-180",
+                        svg {
+                            class: "h-5 w-5 fill-none stroke-current theme1:group-open:stroke-amber-600",
+                            view_box: "0 0 24 24",
+                            path {
+                                stroke_linecap: "round",
+                                stroke_linejoin: "round",
+                                stroke_width: "2",
+                                d: "M19 9l-7 7-7-7"
+                            }
+                        }
+                    }
+                }
+                FullGameLog { day: game.day.unwrap_or_default() }
             }
         }
     }

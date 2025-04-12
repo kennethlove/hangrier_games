14a03443679dcf09442afb20c2a61636460ca6b7
Kenneth Love <kennethlove@gmail.com>
2025-04-12 09:56:59 -0700

    Saving state

--- web/src/components/tribute_detail.rs
+++ web/src/components/tribute_detail.rs
@@ -5,6 +5,7 @@ use dioxus::prelude::*;
 use dioxus_query::prelude::{use_get_query, QueryResult};
 use game::games::Game;
 use game::tributes::Tribute;
+use std::collections::HashMap;
 
 async fn fetch_tribute(keys: Vec<QueryKey>) -> QueryResult<QueryValue, QueryError> {
     if let Some(QueryKey::Tribute(identifier)) = keys.first() {
@@ -35,33 +36,39 @@ async fn fetch_tribute(keys: Vec<QueryKey>) -> QueryResult<QueryValue, QueryErro
 
 #[component]
 pub fn TributeDetail(game_identifier: String, tribute_identifier: String) -> Element {
-    let game_signal: Signal<Option<Game>> = use_context();
+    let mut tribute_game: Game;
 
-    let game = game_signal.read().clone();
-    let game = game.unwrap();
-    let game_identifier = game.identifier.clone();
-
-    let tribute_query = use_get_query(
-        [
-            QueryKey::Tribute(tribute_identifier.clone()),
-            QueryKey::Tributes(game_identifier.clone()),
-            QueryKey::Game(game_identifier.clone()),
-        ],
-        fetch_tribute,
+    let game_query = use_get_query(
+        [QueryKey::Game(game_identifier.clone()), QueryKey::Games],
+        crate::components::game_detail::fetch_game,
     );
 
-    match tribute_query.result().value() {
-        QueryResult::Ok(QueryValue::Tribute(tribute)) => {
-            rsx! {
-                h1 { "{tribute.name}"
+    match game_query.result().value() {
+        QueryResult::Ok(QueryValue::Game(game)) => {
+            let game_identifier = game.identifier.clone();
+
+            let tribute_query = use_get_query(
+                [
+                    QueryKey::Tribute(tribute_identifier.clone()),
+                    QueryKey::Tributes(game_identifier.clone()),
+                    QueryKey::Game(game_identifier.clone()),
+                ],
+                fetch_tribute,
+            );
+
+            match tribute_query.result().value() {
+                QueryResult::Ok(QueryValue::Tribute(tribute)) => {
+                    rsx! {
+                h1 {
+                    "{tribute.name}"
                     TributeEdit {
                         identifier: tribute.clone().identifier,
                         district: tribute.district,
                         name: tribute.clone().name,
                     }
                 }
-                h2 { "{tribute.district}" }
-                h3 { "Location: {tribute.area}" }
+                h2 { "District {tribute.district}" }
+                h3 { "Current location: {tribute.area}" }
                 h3 { "Items" }
                 ul {
                     for item in tribute.clone().items {
@@ -71,15 +78,61 @@ pub fn TributeDetail(game_identifier: String, tribute_identifier: String) -> Ele
                     }
                 }
 
+                h3 { "Attributes" }
+                dl {
+                    class: "grid grid-cols-2 gap-4",
+                    dt { "Health" }
+                    dd { "{tribute.attributes.health}"}
+                    dt { "Sanity" }
+                    dd { "{tribute.attributes.sanity}"}
+                    dt { "Movement" }
+                    dd { "{tribute.attributes.movement}"}
+                    dt { "Strength" }
+                    dd { "{tribute.attributes.strength}"}
+                    dt { "Defense" }
+                    dd { "{tribute.attributes.defense}"}
+                    dt { "Bravery" }
+                    dd { "{tribute.attributes.bravery}"}
+                    dt { "Loyalty" }
+                    dd { "{tribute.attributes.loyalty}"}
+                    dt { "Speed" }
+                    dd { "{tribute.attributes.speed}"}
+                    dt { "Dexterity" }
+                    dd { "{tribute.attributes.dexterity}"}
+                    dt { "Intelligence" }
+                    dd { "{tribute.attributes.intelligence}"}
+                    dt { "Persuasion" }
+                    dd { "{tribute.attributes.persuasion}"}
+                    dt { "Luck" }
+                    dd { "{tribute.attributes.luck}"}
+                    dt { "Hidden?" }
+                    dd { "{tribute.attributes.is_hidden}"}
+                }
+
                 h3 { "Log" }
             }
+                },
+                QueryResult::Err(QueryError::TributeNotFound(identifier)) => {
+                    rsx! { p { "{identifier} not found." } }
+                },
+                QueryResult::Loading(_) => {
+                    rsx! { p { "Loading..." } }
+                },
+                _ => { rsx! { } }
+            }
         }
-        QueryResult::Err(QueryError::TributeNotFound(identifier)) => {
-            rsx! { p { "{identifier} not found." } }
+        QueryResult::Err(_) => {
+            rsx! {
+                p { "Game not found." }
+            }
         }
         QueryResult::Loading(_) => {
-            rsx! { p { "Loading..." } }
+            rsx! {
+                p { "Loading..." }
+            }
         }
-        _ => { rsx! { } }
+        _ => {
+            rsx! {}
+        }
     }
 }

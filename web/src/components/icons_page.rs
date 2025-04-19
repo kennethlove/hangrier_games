b754d8e7ebe013d85c97ac497ea1982d33b7ec90
Kenneth Love <kennethlove@gmail.com>
2025-04-19 09:55:16 -0700

    Enhance icon styles and improve title formatting
    Added size specifications to icon classes for consistent styling. Updated the title to include additional classes for better visual hierarchy. This ensures a more polished and visually appealing UI.

--- web/src/components/icons_page.rs
+++ web/src/components/icons_page.rs
@@ -1,117 +1,120 @@
-use dioxus::prelude::*;
 use crate::components::icons::game_icons_net::*;
+use dioxus::prelude::*;
 
 #[component]
 pub fn IconsPage() -> Element {
     rsx! {
         div {
-            h1 { "Icons" }
+            h1 {
+                class: "text-3xl font-bold",
+                "Icons"
+            }
             div {
                 class: "grid grid-cols-6 gap-4",
                 BrokenBoneIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 BurnedIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 DeadIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 DehydratedIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 DrowningIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 ElectrocutedIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 FallingRocksIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 FishingNetIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 FistIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 FizzingFlaskIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 FrozenBodyIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 HarpoonTridentIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 HealthPotionIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 HeartsIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 HeatHazeIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 HighShotIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 HypodermicTestIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 InfectionIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 MauledIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 PlainDaggerIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 PointySwordIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 PoisonBottleIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 PowderIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 RecentlyDeadIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 ShieldIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 SpearHookIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 SpikedMaceIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 SpinningTopIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 SprayIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 StarvingIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 SwitchbladeIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 TrailMixIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 VomitingIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 WoodAxeIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
                 WoundedIcon {
-                    class: "fill-black",
+                    class: "fill-black size-24",
                 }
             }
         }

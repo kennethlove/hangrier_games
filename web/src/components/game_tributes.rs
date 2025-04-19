0293840a5a196871be82bc600465590f651540c3
Kenneth Love <kennethlove@gmail.com>
2025-04-19 15:39:50 -0700

    Refactor tribute components and improve styling.
    Reorganized layout and styles for tribute cards, including status and item displays. Removed unused code blocks for better readability and streamlined the interface by simplifying the structure of tribute details. Adjusted theme styles for visual consistency across components.

--- web/src/components/game_tributes.rs
+++ web/src/components/game_tributes.rs
@@ -1,4 +1,5 @@
 use crate::cache::{QueryError, QueryKey, QueryValue};
+use crate::components::item_icon::ItemIcon;
 use crate::components::tribute_edit::TributeEdit;
 use crate::components::tribute_status_icon::TributeStatusIcon;
 use crate::routes::Routes;
@@ -8,7 +9,6 @@ use dioxus_query::prelude::{use_get_query, QueryResult};
 use game::games::{Game, GameStatus};
 use game::messages::GameMessage;
 use game::tributes::Tribute;
-use crate::components::item_icon::ItemIcon;
 
 async fn fetch_tributes(keys: Vec<QueryKey>) -> QueryResult<QueryValue, QueryError> {
     if let Some(QueryKey::Tributes(identifier)) = keys.first() {
@@ -117,12 +117,16 @@ pub fn GameTributeListMember(tribute: Tribute) -> Element {
             class: r#"
             border
             p-2
+            self-start
+            relative
+            overflow-hidden
+
             theme1:data-[alive=true]:border-green-500
             theme1:data-[alive=false]:border-red-500
             theme1:text-stone-200
 
-            theme2:data-[alive=true]:border-green-200
-            theme2:data-[alive=false]:border-red-200
+            theme2:data-[alive=true]:border-green-400
+            theme2:data-[alive=false]:border-red-400
             theme2:text-green-200
             theme2:rounded-md
 
@@ -133,11 +137,47 @@ pub fn GameTributeListMember(tribute: Tribute) -> Element {
 
             div {
                 class: r#"
+                absolute
+                inset-0
+                text-right
+
+                theme1:text-amber-200/20
+                theme1:text-8xl
+                theme1:font-[Cinzel]
+                theme2:text-green-200/20
+                theme2:text-8xl
+                theme2:font-[Forum]
+                theme3:text-stone-500/25
+                theme3:text-7xl
+                theme3:font-[Orbitron]
+                "#,
+
+                span {
+                    class: r#"
+                    inline-block
+                    text-sm
+                    transform
+                    -rotate-90
+                    translate-x-1/3
+                    -translate-y-[1.25rem]
+
+                    theme1:translate-x-1/2
+                    theme1:-translate-y-[2rem]
+                    theme1:tracking-widest
+                    theme2:text-base
+                    "#,
+                    "district"
+                },
+
+                "{tribute.district}",
+            }
+
+            div {
+                class: r#"
                 flex
                 flex-row
                 gap-2
                 place-content-between
-                relative
                 "#,
 
                 h4 {
@@ -182,39 +222,6 @@ pub fn GameTributeListMember(tribute: Tribute) -> Element {
                     }
                 }
 
-                div {
-                    class: r#"
-                    absolute
-                    right-0
-                    z-0
-                    theme1:text-amber-200/20
-                    theme1:text-9xl
-                    theme1:font-[Cinzel]
-                    theme2:text-green-200/20
-                    theme2:text-9xl
-                    theme2:font-[Forum]
-                    theme3:text-stone-500/25
-                    theme3:text-8xl
-                    theme3:font-[Orbitron]
-                    "#,
-                    span {
-                        class: r#"
-                        inline-block
-                        text-sm
-                        theme2:text-base
-                        transform
-                        -rotate-90
-                        translate-x-1/3
-                        -translate-y-[1.25rem]
-                        theme1:translate-x-1/2
-                        theme1:-translate-y-[2rem]
-                        theme1:tracking-widest
-                        "#,
-                        "district"
-                    },
-                    "{tribute.district}",
-                }
-
                 if game.status == GameStatus::NotStarted {
                     div {
                         class: "z-1",
@@ -227,74 +234,44 @@ pub fn GameTributeListMember(tribute: Tribute) -> Element {
                 }
             }
 
-            dl {
-                class: "text-sm grid grid-cols-2 gap-2",
-                dt {
-                    "In the",
+            div {
+                class: "text-sm flex flex-row gap-4",
+                p {
+                    "location: ",
+                    span {
+                        class: "font-bold",
+                        "{tribute.area}"
+                    }
                 }
-                dd {
-                    class: "font-bold",
-                    "{tribute.area}"
-                }
-                dt {
-                    "Status",
-                }
-                dd {
+                p {
+                    "status: ",
                     TributeStatusIcon {
                         status: tribute.status.clone(),
                         css_class: r#"
-                        size-5
+                        inline-block
+                        size-6
                         theme1:fill-stone-200
                         theme2:fill-green-200
                         "#
-                    },
-                }
-                dt {
-                    "Health",
-                }
-                dd {
-                    class: "font-bold",
-                    "{tribute.attributes.health}"
+                    }
                 }
             }
 
-            h5 {
-                class: r#"
-                my-2
-                theme1:text-amber-200
-
-                theme2:text-green-200
-                theme2:bg-green-800
-                theme2:px-2
-
-                theme3:border-0
-                theme3:border-b-2
-                theme3:border-gold-rich
-                "#,
-
-                "Items"
-            }
-            if tribute.clone().items.is_empty() {
-                p {
-                    class: "text-sm",
-                    "No items"
-                }
-            } else {
+            if !tribute.clone().items.is_empty() {
                 ul {
                     class: "flex flex-row gap-2 flex-wrap",
                     for item in tribute.clone().items {
                         li {
-                            class: "flex flex-row gap-2 items-center",
                             ItemIcon {
                                 item: item.clone(),
                                 css_class: r#"
-                                size-8
+                                size-6
                                 theme1:fill-amber-500
                                 theme2:fill-green-200
                                 "#,
                             }
                             span {
-                                class: "text-sm",
+                                class: "sr-only",
                                 title: item.to_string(),
                                 "{item.to_string()}"
                             }
@@ -302,40 +279,6 @@ pub fn GameTributeListMember(tribute: Tribute) -> Element {
                     }
                 }
             }
-
-            h5 {
-                class: r#"
-                my-2
-                theme1:text-amber-200
-
-                theme2:text-green-200
-                theme2:bg-green-800
-                theme2:px-2
-
-                theme3:border-0
-                theme3:border-b-2
-                theme3:border-gold-rich
-                "#,
-
-                "Log"
-            }
-            if tribute_logs.is_empty() {
-                p {
-                    class: "text-sm",
-                    "No logs"
-                }
-            } else {
-                ul {
-                    class: "text-sm",
-                    for log in tribute_logs {
-                        li {
-                            class: "mb-2",
-                            "{log.content}"
-                        }
-                    }
-                }
-            }
-
         }
     }
 }
